3a024efcbebaccc4d0706b5376d29cfb5bd96d12
andremorelli <morelliduarte@yahoo.com.br>
2021-03-21 14:54:49 -0300

    switch

---
+++ switch.php
@@ -0,0 +1,28 @@
+<?php
+
+$opcao = 1;
+switch (5) {
+	case 1:
+		echo "Primeiro Case";
+		break;
+
+	case 2:
+		echo "Segundo Case";
+		break;
+
+	case 3:
+		echo "Terceiro Case";
+		break;		
+	
+	default:
+		echo "case default";
+		break;
+}
+
+
+
+
+
+
+
+?>
